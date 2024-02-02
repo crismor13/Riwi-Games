62f6c7997fc7c51c78e717c3bd21b3256db3fd33
Byron927 <byronotalvaro92@gmail.com>
2024-02-02 18:55:54 -0500

    login + ls

---
+++ js/login.js
@@ -0,0 +1,31 @@
+//selectores
+const formLogin = document.getElementById("form-login");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+
+const URLBase = "http://localhost:3000/users";
+
+//Eventos
+formLogin.addEventListener("submit", (e) => {
+  e.preventDefault();
+  login();
+});
+
+async function login() {
+  const response = await fetch(`${URLBase}?email=${email.value}`);
+  const data = await response.json();
+  const user = JSON.stringify(data);
+  console.log(data);
+
+  if (!data.length) {
+    console.log("Email no resgistrado");
+    return;
+  }
+  if (data[0].password === password.value) {
+    window.location.href = "../index.html";//en el caso del proyecto seria enviarlo a home
+    localStorage.setItem("isAuthenticated", "true");
+    localStorage.setItem("user", user);
+  } else {
+    console.log("Credenciales incorrectas");
+  }
+}
