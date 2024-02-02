15c88546c23b0ee0993267a262d17ae60e225abe
Cristian <crismonsalve4@gmail.com>
2024-02-02 12:31:13 -0500

    register.js bug fixed

--- js/register.js
+++ js/register.js
@@ -1,118 +1,86 @@
 //Selectores
 
-const form = document.getElementById("form-register");
-const email = document.getElementById("email")
-const password = document.getElementById("password")
-const passwordConfirmation = document.getElementById("repeat-password")
+let form = document.getElementById("form-register");
+let email;
+let password;
+let passwordConfirmation;
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault();
-    const formData = new FormData(form);
-    const newUser = {};
-  
-    for (const [key, value] of formData) {
-  
-      newUser[key] = value;
-    }
-    // console.log(newUser);
-    // createUser(user);
-    form.reset();
+  email = document.getElementById("email");
+  password = document.getElementById("password").value;
+  passwordConfirmation = document.getElementById("repeat-password").value;
 
-    registerUser(newUser)
-  })
+  e.preventDefault();
+  const formData = new FormData(form);
+  const newUser = {};
 
+  for (const [key, value] of formData) {
+    newUser[key] = value;
+  }
+  // console.log(newUser);
+  // createUser(user);
+  form.reset();
+
+  registerUser(newUser);
+});
 
 async function registerUser(user) {
   //1. La contraseñas tienen que ser iguales
 
   const { validated, message } = validatePassword();
   //2. Contraseña segura
-  console.log(validated, message)
+  console.log(validated, message);
 
   if (!validated) {
-      showAlert(message)
-      return
+    showAlert(message);
+    return;
   }
 
-  const { validated: validatedSegurity, message: messageError } = validatePasswordSegurity();
+  const { validated: validatedSegurity, message: messageError } =
+    validatePasswordSegurity();
 
   if (!validatedSegurity) {
-      showAlert(messageError)
-      return
+    showAlert(messageError);
+    return;
   }
 }
-
 
 function validatePassword() {
-  if (password.value != passwordConfirmation.value) {
-      return { validated: false, message: "Las contraseñas no coinciden" }
+  if (password != passwordConfirmation) {
+    return { validated: false, message: "Las contraseñas no coinciden" };
   }
 
-  return { validated: true }
+  return { validated: true };
 }
-
 
 function validatePasswordSegurity() {
-  const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
+  const regex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,15}$/;
 
   //El metodo test permite evaluar una cadena de texto a partir de una expresión regular
-  if (regex.test(password.value)) {
-      return { validated: true }
+  if (regex.test(password)) {
+    console.log("password: ", password);
+    return { validated: true };
   }
 
-  return { validated: false, message: "La contraseña debe tener mayusculas, minusculas, un caracater especial y un rango de 8 a 15 caracateres" }
-}
+  console.log("password: ", password);
 
+  return {
+    validated: false,
+    message:
+      "La contraseña debe tener mayusculas, minusculas, un caracater especial y un rango de 8 a 15 caracateres",
+  };
+}
 
 function showAlert(message) {
   Swal.fire({
-      title: 'Error!',
-      text: message,
-      icon: 'error',
-      toast: "true",
-      timer: 4000,
-      showConfirmButton: false,
-      position: "bottom-right",
-      confirmButtonText: 'Aceptar'
-  })
+    title: "Error!",
+    text: message,
+    icon: "error",
+    toast: "true",
+    timer: 4000,
+    showConfirmButton: false,
+    position: "bottom-right",
+    confirmButtonText: "Aceptar",
+  });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
