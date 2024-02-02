a84a4e60cca40fa58d8ffaaa52cb53780d3090af
Byron927 <byronotalvaro92@gmail.com>
2024-02-02 07:58:42 -0500

    register

--- js/register.js
+++ js/register.js
@@ -1,19 +1,93 @@
-const form = document.querySelector("form");
+//Selectores
+const URL = 
 
-// form.addEventListener("submit", function (event) {
-//     event.preventDefault();
-//     console.log("object");
-// })
+const form = document.getElementById("form-register");
+const email = document.getElementById("email")
+const password = document.getElementById("password")
+const passwordConfirmation = document.getElementById("repeat-password")
+
+form.addEventListener("submit", (event)=> {
+    event.preventDefault();
+    console.log("object");
+})
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
     const formData = new FormData(form);
-    const user = {};
+    const newUser = {};
   
     for (const [key, value] of formData) {
   
-      user[key] = value;
+      newUser[key] = value;
     }
+    console.log(newUser);
     // createUser(user);
     form.reset();
-  });
+  })
+
+
+async function registerUser() {
+  //1. La contraseñas tienen que ser iguales
+
+  const { validated, message } = validatePassword();
+  //2. Contraseña segura
+  console.log(validated, message)
+
+  if (!validated) {
+      showAlert(message)
+      return
+  }
+
+  const { validated: validatedSegurity, message: messageError } = validatePasswordSegurity();
+
+  if (!validatedSegurity) {
+      showAlert(messageError)
+      return
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
